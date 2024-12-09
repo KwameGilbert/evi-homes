aa8f01d825fd9162da27e011310dd0ef89e23566
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2024-12-09 23:31:47 +0000

    Add Tailwind CSS setup and initial frontend structure

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./frontend/**/*.{html,php,js}",
+    "./backend/**/*.{php,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
